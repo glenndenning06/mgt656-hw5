c6d251ee6854035ce63b198244f16dd44915218d
glenndenning061 <kelly.glenn.g@gmail.com>
2015-10-09 18:35:45 +0000

    Made some really stellar changes to this file, A+ work!

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send('I am a computer. Beep beep boop bop.');
+});
+
+app.get('/excited-raccoon', function(request, response){
+    response.send('I am a computer. Beep beep boop bop. Touch my circuts. Beep beep beep. My hard drive is spinning very rapidly. Ba ba ba beep bap booop booooop.');
 });
 
 app.listen(process.env.PORT || 4000);
